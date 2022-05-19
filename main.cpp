f754ea1cad1a1af5243e39d5fc0262f1037b7cbc
bagrorg <kuldanvit@gmail.com>
2022-05-20 00:34:33 +0300

    + synkhorn doc

--- main.cpp
+++ main.cpp
@@ -10,5 +10,5 @@ int main() {
     std::cout << "\n-----------------\n";
     LinearAlgebra::Algorithms::Experiments::randomExperimentWithZeroesSynkhorn({1, 2, 3, 4, 5, 6, 7}, {5});
     LinearAlgebra::Algorithms::Experiments::randomExperimentWithZeroesSynkhorn({1, 2, 3, 4, 5, 6, 7}, {6, 7, 8, 9});
-    LinearAlgebra::Algorithms::Experiments::randomExperimentWithZeroesAndAddingSynkhorn({1, 2, 3, 4, 5, 6, 7}, {5}, LinearAlgebra::Matrix<double>(0.01, 5, 5));
+    LinearAlgebra::Algorithms::Experiments::randomExperimentWithZeroesAndAddingSynkhorn({1, 2, 3, 4, 5, 6, 7}, {5}, LinearAlgebra::Matrix<double>(0.0001, 5, 5));
 }
